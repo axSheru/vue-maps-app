24258a11a1a2f6062c11cf14b49dbd59052260d6
Alex Paredes <alexparedesr95@gmail.com>
2022-01-02 16:49:00 -0600

    FEAT: Created loading and place to show the map. Also styles of both elements.

--- src/components/mapview/MapView.vue
+++ src/components/mapview/MapView.vue
@@ -1,14 +1,38 @@
 <script lang="ts" src="./MapView.ts" />
 
 <template>
-    <h1>Mapview</h1>
-    <p>
-        {{ isLoading }}
-        {{ userLocation }}
-        {{ isUserLocationReady }}
-    </p>
+    <div v-if="!isUserLocationReady" 
+        class="loading-map d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h3>Espere por favor</h3>
+            <span>Localizando...</span>
+        </div>
+    </div>
+
+    <div
+        v-show="isUserLocationReady"
+        class="map-container"
+        ref="mapElement"
+    />
+
 </template>
 
 <style scoped>
+.loading-map {
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    height: 100vh;
+    left: 0px;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+    z-index: 9999;
+}
 
+.map-container {
+    background-color: red;
+    height: 100vh;
+    position: fixed;
+    width: 100vw;
+}
 </style>
