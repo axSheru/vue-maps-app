e52fc306ae98538e931558c7fb1d21dd03cf8a6c
Alex Paredes <alexparedesr95@gmail.com>
2022-01-03 21:59:22 -0600

    FEAT: Added mapbox access.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import mapboxgl from 'mapbox-gl';
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXhzaGVydSIsImEiOiJja3h6azJiM3QwNHJoMm9sNHZybHk4cDR5In0.eD1Qm9nO7zzFjTgRGjWu8Q';
+
 if ( !navigator.geolocation ) {
     alert("Tu navegador no soporta el GeoLocation");
     throw new Error("Tu navegador no soporta el GeoLocation");
