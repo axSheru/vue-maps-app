6d4f7ebfe848985309e7ad1714060d9c049ba355
Alex Paredes <alexparedesr95@gmail.com>
2022-01-04 17:59:14 -0600

    FEAT: Added location marker and pop up info. Changed map style.

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -17,10 +17,23 @@ export default defineComponent({
 
             const map = new mapboxgl.Map({
                 container: mapElement.value, // container ID
-                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
                 center: userLocation.value, // starting position [lng, lat]
                 zoom: 15 // starting zoom
             });
+
+            const myLocationPopup = new mapboxgl.Popup({ offset: [ 0, -40 ] })
+                .setLngLat( userLocation.value )
+                .setHTML(`
+                    <h4>¡Aquí estoy!</h4>
+                    <p>Actualmente en Puebla</p>
+                    <p>${ userLocation.value }</p>
+                `)
+
+            const myLocationMarker = new mapboxgl.Marker()
+                .setLngLat( userLocation.value )
+                .setPopup( myLocationPopup )
+                .addTo( map );
 
         }
 
