4a5e9ef058c672c29b53a6adf91ef3eb165b6c22
Alex Paredes <alexparedesr95@gmail.com>
2022-01-03 22:00:31 -0600

    FEAT: Added mapbox implementation to show map.

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,6 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { usePlacesStore } from '@/composables';
+import mapboxgl from 'mapbox-gl';
 
 export default defineComponent({
     name: 'MapView',
@@ -7,6 +8,19 @@ export default defineComponent({
 
         const mapElement = ref<HTMLDivElement>();
         const { userLocation, isUserLocationReady } = usePlacesStore();
+
+        const initMap = () => {
+            if ( !mapElement.value ) throw new Error('Elemento Div no existe.');
+            if ( !userLocation.value ) throw new Error('UserLocation no existe.');
+
+            const map = new mapboxgl.Map({
+                container: mapElement.value, // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation.value, // starting position [lng, lat]
+                zoom: 15 // starting zoom
+            });
+
+        }
 
         onMounted(() => {
             console.log(mapElement.value)
