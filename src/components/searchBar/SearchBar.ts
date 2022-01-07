cc258aec4d02a8d0027a30809f750bdca1debd1d
Alex Paredes <alexparedesr95@gmail.com>
2022-01-06 23:53:22 -0600

    FEAT: Created searchBar component that shows a search bar input to search places.

---
+++ src/components/searchBar/SearchBar.ts
@@ -0,0 +1,14 @@
+import { defineComponent } from 'vue';
+import SearchResults from '@/components/searchResults/SearchResults.vue';
+
+export default defineComponent({
+    name: 'SearchBar',
+    components: { SearchResults },
+    setup() {
+
+        return {
+
+        }
+
+    }
+});
