7d2d106eab12d651a23213b9ed0c111b51738d6b
Alex Paredes <alexparedesr95@gmail.com>
2022-01-07 19:20:40 -0600

    FEAT: Created logic with debounce to get the searched value after 2 seconds without typing.

--- src/components/searchBar/SearchBar.ts
+++ src/components/searchBar/SearchBar.ts
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import SearchResults from '@/components/searchResults/SearchResults.vue';
 
 export default defineComponent({
@@ -6,8 +6,26 @@ export default defineComponent({
     components: { SearchResults },
     setup() {
 
-        return {
+        const debounceTimeout = ref();
+        const debouncedValue = ref('');
 
+        return {
+            debouncedValue,
+
+            searchTerm: computed({
+                get() {
+                    return debouncedValue.value;
+                },
+                set( val: string ) {
+                    
+                    if ( debounceTimeout.value ) clearTimeout( debounceTimeout.value );
+
+                    debounceTimeout.value = setTimeout(() => {
+                        debouncedValue.value = val;
+                    }, 2000);
+
+                }
+            })
         }
 
     }
