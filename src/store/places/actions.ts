b4548e9bc9d7ce73ec69dfa64ea5ee61f3c00563
Alex Paredes <alexparedesr95@gmail.com>
2022-01-08 21:46:04 -0600

    FEAT: Made a request to mapbox to get the searched term.

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -1,6 +1,8 @@
 import { ActionTree } from 'vuex';
 import { PlacesState } from './state';
 import { StateInterface } from '../index';
+import { searchApi } from '@/apis/searchAPI';
+import { PlacesResponse } from '@/interfaces/places';
 
 
 const actions: ActionTree<PlacesState, StateInterface> = {
@@ -18,7 +20,15 @@ const actions: ActionTree<PlacesState, StateInterface> = {
 
     // TODO: Colocar el valor de retorno.
     async searchPlacesByTerm({ commit, state }, query: string ) {
-        console.log('Vuex: ', query)
+        
+        const resp = await searchApi.get<PlacesResponse>(`/${ query }.json`, {
+            params: {
+                proximity: state.userLocation?.join(','),
+            }
+        });
+
+        console.log(resp.data);
+
     }
 }
 
