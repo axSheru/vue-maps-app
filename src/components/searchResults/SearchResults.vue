8811ba8595509244a6669dd8a506112748a64930
Alex Paredes <alexparedesr95@gmail.com>
2022-01-10 20:30:58 -0600

    FEAT: Added validations to show search results.

--- src/components/searchResults/SearchResults.vue
+++ src/components/searchResults/SearchResults.vue
@@ -1,32 +1,18 @@
 <script lang="ts" src="./SearchResults.ts" />
 
 <template>
-    <ul class="list-group mt-3">
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <div align="right">
-                <button class="btn btn-outline-primary btn-sm">
-                    Direcciones
-                </button>
-            </div>
-        </li>
-    </ul>
-    <ul class="list-group mt-3">
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <div align="right">
-                <button class="btn btn-outline-primary btn-sm">
-                    Direcciones
-                </button>
-            </div>
-        </li>
-    </ul>
-    <ul class="list-group mt-3">
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <div v-if="isLoadingPlaces"
+        class="alert alert-primary text-center">
+        <h5>Cargando</h5>
+        <span>Espere por favor...</span>
+    </div>
+    <ul v-else-if="places.length > 0"
+        class="list-group mt-3">
+        <li v-for="place in places"
+            :key="place.id"
+            class="list-group-item list-group-item-action">
+            <h5>{{place.text}}</h5>
+            <p>{{place.place_name}}</p>
             <div align="right">
                 <button class="btn btn-outline-primary btn-sm">
                     Direcciones
