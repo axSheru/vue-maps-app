d27bdb3e36642fd001d42f690d3303242fc308ea
Alex Paredes <alexparedesr95@gmail.com>
2022-01-12 21:01:31 -0600

    FEAT: Called mutation setRoutePolyline with the necessary information.

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -1,7 +1,7 @@
 import { ActionTree } from 'vuex';
 import { MapState } from './state';
 import { StateInterface } from '../index';
-import directionsApi from '@/apis/directionsAPI';
+import { directionsApi } from '@/apis';
 import { DirectionsResponse } from '@/interfaces/directions';
 
 export type LngLat = [ number, number ];
@@ -10,7 +10,10 @@ const actions: ActionTree<MapState, StateInterface> = {
     async getRouteBetweenPoints( { commit }, { start, end }: { start: LngLat, end: LngLat }  ) {
         
         const resp = await directionsApi.get<DirectionsResponse>(`${ start.join(',') };${ end.join(',') }`);
-        console.log( resp.data.routes[0].geometry.coordinates );
+        
+        //TODO: State distance.
+        
+        commit( 'setRoutePolyline', resp.data.routes[0].geometry.coordinates );
     }
 }
 
