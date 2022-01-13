806971a122350cd87ad8e1bb7cbc2351a1fd8b13
Alex Paredes <alexparedesr95@gmail.com>
2022-01-12 20:58:06 -0600

    FEAT: Changed map zoom when a marker is selected.

--- src/components/myLocationBtn/MyLocationBtn.vue
+++ src/components/myLocationBtn/MyLocationBtn.vue
@@ -25,7 +25,7 @@ export default defineComponent({
 
                 map.value?.flyTo({
                     center: userLocation.value,
-                    zoom: 14
+                    zoom: 16
                 });
 
             }
