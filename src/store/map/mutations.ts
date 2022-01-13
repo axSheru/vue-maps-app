5646cc9c18b0fee904239df02522748adbae3f8a
Alex Paredes <alexparedesr95@gmail.com>
2022-01-12 21:06:15 -0600

    FEAT: Remove polylines and its information from the map and the state.

--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -37,6 +37,15 @@ const mutation: MutationTree<MapState> = {
             state.markers.push( marker );
 
         }
+
+        // Clear polyline.
+        if ( state.map?.getLayer('RouteString') ) {
+            state.map?.removeLayer('RouteString');
+            state.map?.removeSource('RouteString');
+            state.distance = undefined;
+            state.duration = undefined;
+        }
+
     },
 
     setRoutePolyline( state, coords: number[][] ) {
