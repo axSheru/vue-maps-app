ce340cc142bab17d64de03dec2a24c1d12053046
Alex Paredes <alexparedesr95@gmail.com>
2022-01-12 21:08:36 -0600

    FEAT: Changed number of results to 4.

--- src/apis/searchAPI.ts
+++ src/apis/searchAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const searchApi = axios.create({
     baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
     params: {
-        limit: 5,
+        limit: 4,
         language: 'es',
         access_token: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN,
     }
