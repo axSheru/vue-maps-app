9221a1b800482c7373d3f551669e8a7662595a6b
Alex Paredes <alexparedesr95@gmail.com>
2022-01-15 22:17:42 -0600

    FEAT: Centered distance information div.

--- src/components/tripDetails/TripDetails.vue
+++ src/components/tripDetails/TripDetails.vue
@@ -32,10 +32,10 @@ export default defineComponent({
     bottom: 20px;
     box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.2);
     font-size: 15px;
-    left: 500px;
+    left: 40%;
     padding: 10px;
-    position: fixed;
-    width: 240px;
+    position: absolute;
+    width: 20%;
 }
 
 strong {
